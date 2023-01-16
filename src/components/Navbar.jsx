7482de680f71eb31f6e10fa4cdce9eb9f2deb4bb
Ahmad Raynaldi <achmadreynaldi829@gmail.com>
2023-01-16 14:40:42 +0700

    update code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -56,11 +56,10 @@ function Navbar() {
                         </li>
                     </ul>
                     <div className='w-full flex justify-end gap-4'>
-                        <div className='w-full flex justify-end'>
+                        <div className='w-full flex justify-end bg'>
                             <label
                                 htmlFor='signin'
-                                className='btn btn-primary text-xl'
-                            >
+                                className='btn btn-primary text-xl'>
                                 Sign In
                             </label>
 
@@ -71,17 +70,32 @@ function Navbar() {
                             />
                             <div className='modal modal-bottom sm:modal-middle'>
                                 <div className='modal-box'>
-                                    <h3 className='font-bold text-lg'>
-                                        Congratulations random Internet user!
+                                    <h3 className='font-bold text-lg text-gray-400'>
+                                       Log In
                                     </h3>
-                                    <p className='py-4'>
-                                        You've been selected for a chance to get
-                                        one year of subscription to use
-                                        Wikipedia for free!
-                                    </p>
+                                    <h4 className='text-gray-400'>silahkan masukan NIS & password Anda</h4>
+                             <form action="" method='post' role={'alert'}>
+                                <div className='mb-4'>
+                                <label className='input-group'>
+                                    <input className='w-full py-2 px-3 ' type="NIS" placeholder="NIS" name="email" required/>
+                                </label>
+                                </div>
+                                <div>
+                                <label className='input-group'>
+                                    <input className='w-full py-2 px-3' type="Password" placeholder="Pasword" name="Password" required/>
+                                </label>
+                                <a href="#">
+                                <p className='modal-action text-blue-400 underline'>Lupa Kata sandi?</p>
+                                </a>
+                                </div>
+  
+                            </form>
                                     <div className='modal-action'>
-                                        <label htmlFor='signin' className='btn'>
-                                            Yay!
+                                        <label htmlFor='signin' className='btn bg-blue-500'>
+                                            Submit
+                                        </label>
+                                        <label htmlFor='signin' className='btn bg-red-400'>
+                                            batal
                                         </label>
                                     </div>
                                 </div>
